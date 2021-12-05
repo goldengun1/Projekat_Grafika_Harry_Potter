cbfe1bc78b1cf5dcc284fa911a4aa5d3df5125d2
Stefanija Markovic <stefanija.mar1999@gmail.com>
2021-12-05 20:39:09 +0100

    added textures for skybox

--- src/main.cpp
+++ src/main.cpp
@@ -719,7 +719,15 @@ unsigned int loadCubemap(vector<string> faces)
     for( int i = 0; i < faces.size(); ++i){
         data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
         if (data){
-            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            GLenum format;
+            if (nrChannels == 1)
+                format = GL_RED;
+            else if (nrChannels == 3)
+                format = GL_RGB;
+            else if (nrChannels == 4)
+                format = GL_RGBA;
+
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         }else{
             std::cerr<<"FAILED TO LOAD CUBE MAP TEXTURE FACE"<<std::endl;
             return -1;
